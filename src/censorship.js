4e370801388ab246e597ecd0ec10cc1ab8f51f05
Alex Spidola <AlexeyYartsev@hotmail.com>
2024-10-31 21:59:08 +0200

    feat: hometask-03 3/4

--- src/censorship.js
+++ src/censorship.js
@@ -14,5 +14,7 @@
  * @returns {function}
  */
 module.exports.censorship = function censorship(forbidden) {
-  throw new Error('Not implemented'); // remove me and write a solution
+  return function getCensored(str) {
+    return forbidden.sort((a, b) => b.length - a.length).reduce((acc, el) => acc.replaceAll(el, '*'.repeat(el.length)), str);
+  };
 };
--- src/formatAddress.js
+++ src/formatAddress.js
@@ -4,5 +4,9 @@
  * @returns {function}
  */
 module.exports.formatAddress = function formatAddress() {
-  throw new Error('Not implemented'); // remove me and write a solution
+  return function fetFormatedAddress(objAddress) {
+    return Object.keys(objAddress)
+      .reduce((acc, key) => acc.concat([objAddress[key]]), [])
+      .join(', ');
+  };
 };
