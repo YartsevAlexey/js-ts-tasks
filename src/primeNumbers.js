47798534814684d454bea0da8c83a6bce857e9cb
Alex Spidola <AlexeyYartsev@hotmail.com>
2024-11-01 17:05:06 +0200

    feat: add solution hometask-03-functions

--- src/primeNumbers.js
+++ src/primeNumbers.js
@@ -4,5 +4,24 @@
  * @returns {function}
  */
 module.exports.primeNumbers = function primeNumbers(highestNumber) {
-  throw new Error('Not implemented'); // remove me and write a solution
+  return function sieveOfEratosthenes(minValue, maxValue) {
+    // Eratosphen optimized
+    const arrNotPrime = [];
+    for (let i = 2; i <= Math.min(highestNumber, maxValue); i++) {
+      if (arrNotPrime[i] !== true) {
+        for (let j = i ** 2; j <= highestNumber; j += i) {
+          // if (j >= minValue) // if optimization by memory
+          arrNotPrime[j] = true; // if optimization by speed
+        }
+      }
+    }
+
+    const arrPrime = [];
+    for (let k = minValue; k <= maxValue; k++) {
+      if (arrNotPrime[k] === undefined) {
+        arrPrime.push(k);
+      }
+    }
+    return arrPrime;
+  };
 };
