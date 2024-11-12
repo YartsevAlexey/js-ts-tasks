c70175f7d0bed53a565266a79af726e9f3e633aa
Alex Spidola <AlexeyYartsev@hotmail.com>
2024-11-12 22:44:07 +0200

    fix: optimized

--- src/user.js
+++ src/user.js
@@ -3,63 +3,45 @@ class User {
   #secondName;
   #age;
 
+  #getTypedValue(value, type) {
+    if (typeof value !== type) {
+      throw new Error(`Wrong value type "${value}" - should be "${type}`);
+    }
+    return value;
+  }
+
   constructor(newFirstName, newSecondName, newAge) {
-    // if (newFirstName === undefined || newSecondName === undefined || newAge === undefined) {
-    //   throw new Error('Empty data');
-    // }
-
-    this.#typeTest(newFirstName, 'string');
-    this.#typeTest(newSecondName, 'string');
-    this.#typeTest(newAge, 'number');
-
-    this.#firstName = newFirstName;
-    this.#secondName = newSecondName;
-    this.#age = newAge;
+    this.firstName = newFirstName;
+    this.secondName = newSecondName;
+    this.age = newAge;
   }
 
   set firstName(newFirstName) {
-    this.#typeTest(newFirstName, 'string');
-    this.#firstName = newFirstName;
-  }
-  set secondName(newSecondName) {
-    this.#typeTest(newSecondName, 'string');
-    this.#secondName = newSecondName;
-  }
-  set age(newAge) {
-    this.#typeTest(newAge, 'number');
-    this.#age = newAge;
+    this.#firstName = this.#getTypedValue(newFirstName, 'string');
   }
 
-  get firstName() {
-    return this.#firstName;
+  set secondName(newSecondName) {
+    this.#secondName = this.#getTypedValue(newSecondName, 'string');
   }
-  get secondName() {
-    return this.#secondName;
+
+  set age(newAge) {
+    this.#age = this.#getTypedValue(newAge, 'number');
   }
+
   get age() {
     return this.#age;
   }
+
   get name() {
     return `${this.#firstName} ${this.#secondName}`;
   }
 
-  #typeTest(value, type) {
-    if (typeof value !== type) {
-      throw new Error(`Wrong value type - should be "${type}"`);
-    }
-  }
-
-  static fromObject(obj) {
-    return new User(obj.firstName, obj.secondName, obj.age);
-  }
-
   introduce() {
-    return `My name is ${this.#firstName} ${this.#secondName}, I'm ${this.#age}`;
+    return `My name is ${this.name}, I'm ${this.#age}`;
   }
 
   celebrateBirthday() {
-    this.#age += 1;
-    return this;
+    return this.#age++, this;
   }
 }
 
@@ -86,7 +68,8 @@ module.exports.createUser = function createUser(firstName, secondName, age) {
  * @returns {Array<User>}
  */
 module.exports.createUsers = function createUsers(data) {
-  return data.reduce((acc, el) => acc.concat(User.fromObject(el)), []);
+  //  return data.reduce((acc, el) => acc.concat(User.fromObject(el)), []);
+  return data.map(el => new User(el.firstName, el.secondName, el.age));
 };
 
 /**
@@ -116,6 +99,5 @@ module.exports.createUsersSortFn = function (TestUtils) {
  * @return {Array<User>}
  */
 module.exports.celebrate = function (users) {
-  const u = users.map((el, idx) => (idx % 2 === 0 ? el.celebrateBirthday() : el));
-  return u;
+  return users.map((el, idx) => (idx % 2 === 0 ? el.celebrateBirthday() : el));
 };
