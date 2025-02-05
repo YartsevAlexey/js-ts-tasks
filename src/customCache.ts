aafc24470b8c66ba63cafbf7ce5bb43a8c582516
Alex Spidola <AlexeyYartsev@hotmail.com>
2025-02-05 23:55:28 +0200

    feat: hometask-08-typescript-classes

--- src/customCache.ts
+++ src/customCache.ts
@@ -1,3 +1,48 @@
+type Queue<K> = [K, number][];
+
 export default class CustomCache<K, V> {
-  // your code here
+  private cache: Map<K, V> = new Map();
+
+  private queue: Queue<K> = [];
+
+  private maxSize: number;
+
+  constructor(maxSize: number) {
+    this.maxSize = maxSize;
+  }
+
+  set(key: K, value: V): void {
+    if (this.cache.has(key)) {
+      this.cache.set(key, value);
+    } else {
+      if (this.cache.size === this.maxSize) {
+        if (this.queue[0] !== undefined) {
+          this.cache.delete(this.queue[0][0]);
+          this.queue.shift();
+        }
+      }
+      this.queue.push([key, 0]);
+      this.cache.set(key, value);
+    }
+  }
+
+  get(key: K): V | undefined {
+    if (this.cache.has(key)) {
+      const item = this.queue.find(el => el[0] === key);
+      if (item !== undefined) {
+        item[1] += 1;
+      }
+      this.queue.sort((a, b) => a[1] - b[1]);
+      return this.cache.get(key);
+    }
+    return undefined;
+  }
+
+  size(): number {
+    return this.cache.size;
+  }
+
+  clear(): void {
+    this.cache.clear();
+  }
 }
